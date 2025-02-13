ffb3977cc8edf970df9cba88cc7755056bbd28f2
Shane Duffy <140999121+duffy-01@users.noreply.github.com>
2025-02-13 12:05:12 -0500

    Update instruction_rom.sv

--- code/instruction_rom.sv
+++ code/instruction_rom.sv
@@ -4,7 +4,7 @@
 // Class: ECE 363 - Design of Digital Systems
 // Assignment: PRD Option 1
 // Due date: (Initial submission) Friday, 21 February 2025 @ 23:59
-//Update alu.sv with header
+//
 // Purpose of program: Implement basic instruction set.
 //
 // Filename: instruction_rom.sv
