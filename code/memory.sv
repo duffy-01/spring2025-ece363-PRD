b981851f17ffd5d7748632fe29a504a3a7d6ea2c
Shane Duffy <140999121+duffy-01@users.noreply.github.com>
2025-02-13 11:48:46 -0500

    Create memory.sv and add header.

---
+++ code/memory.sv
@@ -0,0 +1,14 @@
+//*************************************************************************
+// Author(s): Liam Burke, Shane Duffy
+// Creation Date: Thursday, 13 February 2025
+// Class: ECE 363 - Design of Digital Systems
+// Assignment: PRD Option 1
+// Due date: (Initial submission) Friday, 21 February 2025 @ 23:59
+//
+// Purpose of program: Implement RAM.
+//
+// Filename: memory.sv
+// Additional files needed: N/A
+//
+// Date of last modification: Thursday, 13 February 2025 @ 11:47
+//*************************************************************************
