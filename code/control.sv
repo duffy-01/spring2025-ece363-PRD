50c46d44c2987c1fc98cee4a35d7663f1f2e80b7
Shane Duffy <140999121+duffy-01@users.noreply.github.com>
2025-02-13 11:50:46 -0500

    Update control.sv header

--- code/control.sv
+++ code/control.sv
@@ -1,1 +1,14 @@
-
+//*************************************************************************
+// Author(s): Liam Burke, Shane Duffy
+// Creation Date: Tuesday, 11 February 2025
+// Class: ECE 363 - Design of Digital Systems
+// Assignment: PRD Option 1
+// Due date: (Initial submission) Friday, 21 February 2025 @ 23:59
+//
+// Purpose of program: Implement ALU controller. Handles instructions and memory.
+//
+// Filename: control.sv
+// Additional files needed: N/A
+//
+// Date of last modification: Thursday, 13 February 2025 @ 11:50
+//*************************************************************************
